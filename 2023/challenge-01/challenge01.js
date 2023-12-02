0d33047414ec876186d1af4840a771ec713e661f
Angel Sanchez Tenjo <ajsanchez22@gmail.com>
2023-12-02 04:45:51 +0000

    Solution Challenge 1

---
+++ 2023/challenge-01/challenge01.js
@@ -0,0 +1,15 @@
+function findFirstRepeated(gifts) {
+    const stock = new Map();
+
+    for (const gitftId of gifts) {
+
+        if (stock.has(gitftId)) {
+
+            return gitftId;
+        }
+
+        stock.set(gitftId, 1);
+    }
+
+    return -1;
+}
