6b68d2da69a8ace05ade46060313b95b5ca1ee59
Angel Sanchez Tenjo <ajsanchez22@gmail.com>
2023-12-05 05:29:10 +0000

    ✅ Solution Challenge 02

--- 2021/challenge-02/challenge02.js
+++ 2021/challenge-02/challenge02.js
@@ -1,13 +1,21 @@
 function listGifts(letter) {
     
     const words = letter.split(' ');
+    const gifts = new Map();
 
     for(let word of words) {
-        if(word && word.trim().length > 0) {
-            
+        if(word && word.trim().length > 0 && word[0] !== '_') {
+            console.log(word);
+            if(gifts.has(word)) {
+                gifts.set(word,gifts.get(word) + 1);
+            } else {
+                gifts.set(word,1);
+            }
         }
     }
-    return {}
+
+
+    return Object.fromEntries(gifts);
 }
 
 module.exports = listGifts;
--- 2021/challenge-02/challenge02.test.js
+++ 2021/challenge-02/challenge02.test.js
@@ -12,8 +12,23 @@ describe("Challenge 02 # ¡Ayuda al elfo a listar los regalos!", () => {
             peluche: 1
         }
 
+        console.log(typeof expected);
         console.log(regalos);
 
+        expect(typeof regalos).toEqual('object');
         expect(regalos).toEqual(expected);
     });
+
+    it("Test#02: listGifts('_bici _ a m __ b c a b c ')", () => {
+        const carta = '_bici _ a m __ b c a b c ';
+        const regalos = listGifts(carta);
+
+        const expected = { "a": 2, "m": 1, "b": 2, "c": 2 }
+
+        console.log(regalos);
+
+        expect(typeof regalos).toEqual('object');
+        expect(regalos).toEqual(expected);
+    });
+
 });
