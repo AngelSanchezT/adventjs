91b7b7617ae8f4c39f2835dd5f83b0c030057487
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-15 23:11:14 -0500

    Initial Challenge 14

---
+++ 2023/challenge-14/challenge14.js
@@ -0,0 +1,3 @@
+function maxGifts(houses) {
+    return 0
+}
---
+++ 2023/challenge-14/challenge14.test.js
@@ -0,0 +1,80 @@
+/**
+
+
+Test #01
+Test #02
+Test: maxGifts([1, 2, 3, 1])
+
+Expected:
+4
+
+Actual:
+0
+Test #03
+Test: maxGifts([2, 7, 9, 3, 1])
+
+Expected:
+12
+
+Actual:
+0
+Test #04
+Test: maxGifts([0, 0, 0, 0, 1])
+
+Expected:
+1
+
+Actual:
+0
+Test #05
+Test: maxGifts([100])
+
+Expected:
+100
+
+Actual:
+0
+Test #06
+Test: maxGifts([1, 1, 1, 1])
+
+Expected:
+2
+
+Actual:
+0
+Test #07
+Test: maxGifts([1, 1, 1])
+
+Expected:
+2
+
+Actual:
+0
+Test #08
+Test: maxGifts([3, 4, 5])
+
+Expected:
+8
+
+Actual:
+0
+Test #09
+Test: maxGifts([99])
+
+Expected:
+99
+
+Actual:
+0
+Test #10 secret
+Test #11 secret
+Test #12 secret
+Test #13 secret
+Test #14 secret
+Test #15 secret
+Test #16 secret
+Test #17 secret
+Test #18 secret
+Test #19 secret
+
+ */
