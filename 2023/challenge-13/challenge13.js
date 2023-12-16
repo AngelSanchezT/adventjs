19c51bc48cb4e872226a39886e1c6bf47b9525ad
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-15 23:06:39 -0500

    Initial Challenge 13

---
+++ 2023/challenge-13/challenge13.js
@@ -0,0 +1,3 @@
+function calculateTime(deliveries) {
+    return '00:00:00'
+}
---
+++ 2023/challenge-13/challenge13.test.js
@@ -0,0 +1,69 @@
+
+/*
+
+Test #01
+Test: return type
+
+Expected:
+"string"
+
+Actual:
+"string"
+Test #02
+Test: calculateTime(['00:10:00', '01:00:00', '03:30:00'])
+
+Expected:
+"-02:20:00"
+
+Actual:
+"00:00:00"
+Test #03
+Test: calculateTime(['01:00:00', '05:00:00', '00:30:00'])
+
+Expected:
+"-00:30:00"
+
+Actual:
+"00:00:00"
+Test #04
+Test: calculateTime(['02:00:00', '05:00:00', '00:30:00'])
+
+Expected:
+"00:30:00"
+
+Actual:
+"00:00:00"
+Test #05
+Test: calculateTime(['00:45:00', '00:45:00', '00:00:30', '00:00:30'])
+
+Expected:
+"-05:29:00"
+
+Actual:
+"00:00:00"
+Test #06
+Test #07
+Test: calculateTime(['01:01:01', '09:59:59', '01:01:01'])
+
+Expected:
+"05:02:01"
+
+Actual:
+"00:00:00"
+Test #08
+Test: calculateTime(['01:01:01', '03:59:59', '01:01:01', '00:57:58'])
+
+Expected:
+"-00:00:01"
+
+Actual:
+"00:00:00"
+Test #09 secret
+Test #10 secret
+Test #11 secret
+Test #12 secret
+Test #13 secret
+Test #14 secret
+Test #15 secret
+
+*/
