1ccfab884f4dd9e9aa363999b8e5ff60dd3b6c8a
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-15 23:20:26 -0500

    Initial Challenge 15

---
+++ 2023/challenge-15/challenge15.test.js
@@ -0,0 +1,86 @@
+/*
+Tests failed
+Test #01
+Test #02
+Test #03
+Test #04
+Test: autonomousDrive(
+[
+  '..!....',
+  '......*'
+],
+['R', 'D', 'L'])
+
+Expected:
+[
+  ".......",
+  "..!...*"
+]
+
+Actual:
+[
+  "..!....",
+  "......*"
+]
+Test #05
+Test: autonomousDrive(
+[
+  '*..!..*',
+  '*.....*'
+],
+['R', 'R', 'R', 'D', 'D'])
+
+Expected:
+[
+  "*.....*",
+  "*....!*"
+]
+
+Actual:
+[
+  "*..!..*",
+  "*.....*"
+]
+Test #06
+Test: autonomousDrive(['***', '.!.', '***'], ['D', 'U' 'R', 'R', 'R'])
+
+Expected:
+[
+  "***",
+  "..!",
+  "***"
+]
+
+Actual:
+[
+  "***",
+  ".!.",
+  "***"
+]
+Test #07
+Test #08
+Test: autonomousDrive(
+[
+  '.**.*.*.',
+  '.***....',
+  '..!.....'
+], ['D', 'U', 'R', 'R', 'R'])
+
+Expected:
+[
+  ".**.*.*.",
+  ".***....",
+  ".....!.."
+]
+
+Actual:
+[
+  ".**.*.*.",
+  ".***....",
+  "..!....."
+]
+Test #09 secret
+Test #10 secret
+Test #11 secret
+
+*/
