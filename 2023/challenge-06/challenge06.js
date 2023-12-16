a101fbea40734aea3e489a63ba615f4bf7434c9a
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-16 02:03:41 -0500

    :white_check_mark: Solution Challenge 6

--- 2023/challenge-06/challenge06.js
+++ 2023/challenge-06/challenge06.js
@@ -1,6 +1,13 @@
 function maxDistance(movements) {
-    // Code here
-    return 0
+    const RIGHT = '>';
+    const LEFT = '<';
+    const MOVE = '*';
+
+    let moveRight = movements.split(RIGHT).length - 1;
+    let moveLeft = movements.split(LEFT).length - 1;
+    let move = movements.split(MOVE).length - 1;
+
+    return  Math.abs(moveRight - moveLeft) + move;
 }
 
 module.exports = maxDistance;
--- 2023/challenge-06/challenge06.test.js
+++ 2023/challenge-06/challenge06.test.js
@@ -3,41 +3,41 @@ const maxDistance = require('./challenge06');
 describe("Challenge #6: 🦌 The reindeer on trial", () => {
     it("Test #01: return type | Expected: 'number'", () => {
         const result =  maxDistance(">>*<");
-        expect(typeof result),toEqual("number");
+        expect(typeof result).toEqual("number");
     });
 
     it("Test #02: maxDistance('>>*<') | Expected: 2", () => {
         const result =  maxDistance(">>*<");
         const expected = 2;
 
-        expect(result),toEqual(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #03: maxDistance('<<<<<') | Expected: 5", () => {
         const result =  maxDistance("<<<<<");
         const expected = 5;
 
-        expect(result),toEqual(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #04: maxDistance('>***>') | Expected: 5", () => {
         const result =  maxDistance(">***>");
         const expected = 5;
 
-        expect(result),toEqual(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #05: maxDistance('**********') | Expected: 10", () => {
         const result =  maxDistance("**********");
         const expected = 10;
 
-        expect(result),toEqual(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #06: maxDistance('<<**>>') | Expected: 2", () => {
         const result =  maxDistance("<<**>>");
         const expected = 2;
 
-        expect(result),toEqual(expected);
+        expect(result).toEqual(expected);
     });
 });
