e0bf4991fb030a6891c1fde98ebfda77ce4e4540
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-18 22:55:17 -0500

    Initial Challenge 17

---
+++ 2023/challenge-17/challenge17.test.js
@@ -0,0 +1,131 @@
+/*
+Tests failed
+Test #01
+Test: return type
+
+Expected:
+"array"
+
+Actual:
+"array"
+Test #02
+Test: optimizeIntervals([[2, 7], [3, 4], [5, 8]])
+
+Expected:
+[
+  [
+    2,
+    8
+  ]
+]
+
+Actual:
+[]
+Test #03
+Test: optimizeIntervals([[3, 4], [5, 8], [2, 7]])
+
+Expected:
+[
+  [
+    2,
+    8
+  ]
+]
+
+Actual:
+[]
+Test #04
+Test: optimizeIntervals([[1, 3], [2, 6], [8, 10]])
+
+Expected:
+[
+  [
+    1,
+    6
+  ],
+  [
+    8,
+    10
+  ]
+]
+
+Actual:
+[]
+Test #05
+Test: optimizeIntervals([[1, 2], [3, 4], [5, 6]])
+
+Expected:
+[
+  [
+    1,
+    2
+  ],
+  [
+    3,
+    4
+  ],
+  [
+    5,
+    6
+  ]
+]
+
+Actual:
+[]
+Test #06
+Test: optimizeIntervals([[5, 7], [6, 8]])
+
+Expected:
+[
+  [
+    5,
+    8
+  ]
+]
+
+Actual:
+[]
+Test #07
+Test: optimizeIntervals([[1, 5], [6, 10], [11, 15], [16, 20]])
+
+Expected:
+[
+  [
+    1,
+    5
+  ],
+  [
+    6,
+    10
+  ],
+  [
+    11,
+    15
+  ],
+  [
+    16,
+    20
+  ]
+]
+
+Actual:
+[]
+Test #08
+Test: optimizeIntervals([[1, 15], [8, 12], [4, 7]])
+
+Expected:
+[
+  [
+    1,
+    15
+  ]
+]
+
+Actual:
+[]
+Test #09 secret
+Test #10 secret
+Test #11 secret
+Test #12 secret
+Test #13 secret
+*/
