fd62ae4050f067b63b712aa253926f0b42e40040
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-20 23:41:45 -0500

    Initial Challenge 20

---
+++ 2023/challenge-20/challenge20.test.js
@@ -0,0 +1,229 @@
+/*
+Test #01
+Test #02
+Test: distributeGifts([
+  [4, 5, 1],
+  [6, null, 3],
+  [8, null, 4]
+])
+
+Expected:
+[
+  [
+    5,
+    3,
+    3
+  ],
+  [
+    6,
+    5,
+    3
+  ],
+  [
+    7,
+    6,
+    4
+  ]
+]
+
+Actual:
+[
+  [
+    4,
+    5,
+    1
+  ],
+  [
+    6,
+    null,
+    3
+  ],
+  [
+    8,
+    null,
+    4
+  ]
+]
+Test #03
+Test: distributeGifts([
+  [2, null],
+  [null, 3]
+])
+
+Expected:
+[
+  [
+    2,
+    3
+  ],
+  [
+    3,
+    3
+  ]
+]
+
+Actual:
+[
+  [
+    2,
+    null
+  ],
+  [
+    null,
+    3
+  ]
+]
+Test #04
+Test: distributeGifts([
+  [2, 1, 1],
+  [3, 4, null]
+])
+
+Expected:
+[
+  [
+    2,
+    2,
+    1
+  ],
+  [
+    3,
+    3,
+    3
+  ]
+]
+
+Actual:
+[
+  [
+    2,
+    1,
+    1
+  ],
+  [
+    3,
+    4,
+    null
+  ]
+]
+Test #05
+Test: distributeGifts([
+  [null, 5],
+  [3, null]
+])
+
+Expected:
+[
+  [
+    4,
+    5
+  ],
+  [
+    3,
+    4
+  ]
+]
+
+Actual:
+[
+  [
+    null,
+    5
+  ],
+  [
+    3,
+    null
+  ]
+]
+Test #06
+Test: distributeGifts([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+])
+
+Expected:
+[
+  [
+    2,
+    3,
+    4
+  ],
+  [
+    4,
+    5,
+    6
+  ],
+  [
+    6,
+    7,
+    8
+  ]
+]
+
+Actual:
+[
+  [
+    1,
+    2,
+    3
+  ],
+  [
+    4,
+    5,
+    6
+  ],
+  [
+    7,
+    8,
+    9
+  ]
+]
+Test #07
+Test: distributeGifts([
+  [null, 1, null, 1, null],
+  [1, null, 1, null, 1],
+])
+
+Expected:
+[
+  [
+    1,
+    1,
+    1,
+    1,
+    1
+  ],
+  [
+    1,
+    1,
+    1,
+    1,
+    1
+  ]
+]
+
+Actual:
+[
+  [
+    null,
+    1,
+    null,
+    1,
+    null
+  ],
+  [
+    1,
+    null,
+    1,
+    null,
+    1
+  ]
+]
+Test #08 secret
+Test #09 secret
+Test #10 secret
+Test #11 secret
+
+
+*/
