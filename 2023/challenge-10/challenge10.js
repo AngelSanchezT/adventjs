ca0ee1e683cd05ef2b097428514f81d6a7d4634d
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-22 08:33:08 -0500

    ✅ Solution Challenge 10

--- 2023/challenge-10/challenge10.js
+++ 2023/challenge-10/challenge10.js
@@ -1,5 +1,31 @@
 function createChristmasTree(ornaments, height) {
-    return '|'
+  const ESPACE = " ";
+  const ENTER = "\n";
+  const BASE = "|";
+  const ornamentsList = ornaments.repeat(height * 3).split("");
+  let tree = "";
+
+  let totalOrnament = ornamentsList.join(ESPACE);
+
+  tree += ESPACE.repeat(height - 1) + totalOrnament.substring(0, 1) + ENTER;
+  let numOrnaments = 1;
+  let positionI = 0;
+  let positionF = 0;
+
+  for (let i = 2; i <= height; i++) {
+    positionI = numOrnaments * 2;
+    positionF = (numOrnaments + i) * 2 - 1;
+
+    tree += ESPACE.repeat(height - i);
+    tree += totalOrnament.substring(positionI, positionF);
+    tree += ENTER;
+
+    numOrnaments += i;
+  }
+
+  tree += ESPACE.repeat(height - 1) + BASE + ENTER;
+
+  return tree;
 }
 
 module.exports = createChristmasTree;
--- 2023/challenge-10/challenge10.test.js
+++ 2023/challenge-10/challenge10.test.js
@@ -1,16 +1,44 @@
 const createChristmasTree = require('./challenge10');
 
 describe("Challenge #10: 🎄 Create your own Christmas tree", () => {
-    it("Test #01: ", () => {
+    it("Test #01: return type | Expected: 'string'", () => {
         const result = createChristmasTree("123", 4);
-        const expected = "    1\n        2 3\n       1 2 3\n     1 2 3 1 2\n         |\n";
+        const expected = "string";
+
+        expect(typeof result).toEqual(expected);
+    });
+
+    it('Test #02: createChristmasTree("x", 3)', () => {
+        const result = createChristmasTree("x", 3);
+        const expected = "  x\n x x\nx x x\n  |\n";
 
         expect(result).toEqual(expected);
     });
 
-    it("Test #02: ", () => {
+    it('Test #03: createChristmasTree("xo", 4)', () => {
+        const result = createChristmasTree("xo", 4);
+        const expected = "   x\n  o x\n o x o\nx o x o\n   |\n";
+        
+        expect(result).toEqual(expected);
+    });
+
+    it('Test #04: createChristmasTree("123", 5)', () => {
+        const result = createChristmasTree("123", 5);
+        const expected = "    1\n   2 3\n  1 2 3\n 1 2 3 1\n2 3 1 2 3\n    |\n";
+        
+        expect(result).toEqual(expected);
+    });
+
+    it('Test #05: createChristmasTree("*@o", 3)', () => {
         const result = createChristmasTree("*@o", 3);
-        const expected = "  *\n        @ o\n       * @ o\n         |\n";
+        const expected = "  *\n @ o\n* @ o\n  |\n";
+        
+        expect(result).toEqual(expected);
+    });
+
+    it('Test #06: createChristmasTree("123", 4)', () => {
+        const result = createChristmasTree("123", 4);
+        const expected = "   1\n  2 3\n 1 2 3\n1 2 3 1\n   |\n";
 
         expect(result).toEqual(expected);
     });
