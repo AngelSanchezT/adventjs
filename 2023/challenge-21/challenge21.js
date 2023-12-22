c0973813a3a0176a31eaa02da7f58c05baf2461d
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-21 23:30:32 -0500

    Initial Challenge 21

---
+++ 2023/challenge-21/challenge21.js
@@ -0,0 +1,3 @@
+function findBalancedSegment(message) {
+  return [];
+}
---
+++ 2023/challenge-21/challenge21.test.js
@@ -0,0 +1,95 @@
+/*
+Test #01
+Test: return type
+
+Expected:
+"array"
+
+Actual:
+"array"
+Test #02
+Test: findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1])
+
+Expected:
+[
+  2,
+  5
+]
+
+Actual:
+[]
+Test #03
+Test: findBalancedSegment([1, 1, 0])
+
+Expected:
+[
+  1,
+  2
+]
+
+Actual:
+[]
+Test #04
+Test #05
+Test: findBalancedSegment([1, 0, 1])
+
+Expected:
+[
+  0,
+  1
+]
+
+Actual:
+[]
+Test #06
+Test: findBalancedSegment([1, 0, 1, 0])
+
+Expected:
+[
+  0,
+  3
+]
+
+Actual:
+[]
+Test #07
+Test: findBalancedSegment([1, 1, 0, 1, 0, 1])
+
+Expected:
+[
+  1,
+  4
+]
+
+Actual:
+[]
+Test #08
+Test: findBalancedSegment([1, 0, 0, 0, 1, 1, 1, 0, 0, 0])
+
+Expected:
+[
+  0,
+  7
+]
+
+Actual:
+[]
+Test #09
+Test: findBalancedSegment([0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1])
+
+Expected:
+[
+  5,
+  10
+]
+
+Actual:
+[]
+Test #10 secret
+Test #11 secret
+Test #12 secret
+Test #13 secret
+Test #14 secret
+Test #15 secret
+
+*/
