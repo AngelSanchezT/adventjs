e9773b865510648535703ce4393c62474f172c32
Angel Sanchez Tenjo <ajsanchez22@gmail.com>
2023-12-23 19:33:03 +0000

    Initial Challenge 22

---
+++ 2023/challenge-22/challenge22.test.js
@@ -0,0 +1,101 @@
+
+/* 
+
+Han fallado los tests
+Test #01
+Test: return type
+
+Expected:
+"number"
+
+Actual:
+"number"
+Test #02
+Test: compile('++*-')
+
+Expected:
+3
+
+Actual:
+0
+Test #03
+Test: compile('++¿+?')
+
+Expected:
+3
+
+Actual:
+0
+Test #04
+Test #05
+Test: compile('++*¿-?')
+
+Expected:
+3
+
+Actual:
+0
+Test #06
+Test: compile('++%++<')
+
+Expected:
+6
+
+Actual:
+0
+Test #07
+Test: compile('++%++<++¿*?')
+
+Expected:
+16
+
+Actual:
+0
+Test #08
+Test: compile('++¿+?¿+?¿+?')
+
+Expected:
+5
+
+Actual:
+0
+Test #09
+Test: compile('--¿+++?')
+
+Expected:
+-2
+
+Actual:
+0
+Test #10
+Test: compile('--¿+++?+++¿--?')
+
+Expected:
+-1
+
+Actual:
+0
+Test #11
+Test: compile('<%+¿++%++<?')
+
+Expected:
+7
+
+Actual:
+0
+Test #12
+Test: compile('<<<<<<+<<<<<+%')
+
+Expected:
+2
+
+Actual:
+0
+Test #13 secret
+Test #14 secret
+Test #15 secret
+Test #16 secret
+Test #17 secret
+
+
+*/
