dd062729578406b5bba45bafde7c41c90d9f6ca8
Angel Sanchez Tenjo <ajsanchez22@gmail.com>
2023-12-23 19:41:43 +0000

    Initial Challenge 23

---
+++ 2023/challenge-23/challenge23.js
@@ -0,0 +1,3 @@
+function organizeChristmasDinner(dishes) {
+    return []
+}
---
+++ 2023/challenge-23/challenge23.test.js
@@ -0,0 +1,173 @@
+
+
+/*
+Tests failed
+Test #01
+Test: return type
+
+Expected:
+"array"
+
+Actual:
+"array"
+Test #02
+Test: organizeChristmasDinner([
+        ["christmas turkey", "turkey", "sauce", "herbs"],
+        ["cake", "flour", "sugar", "egg"],
+        ["hot chocolate", "chocolate", "milk", "sugar"],
+        ["pizza", "sauce", "tomato", "cheese", "ham"]
+      ])
+
+Expected:
+[
+  [
+    "sauce",
+    "christmas turkey",
+    "pizza"
+  ],
+  [
+    "sugar",
+    "cake",
+    "hot chocolate"
+  ]
+]
+
+Actual:
+[]
+Test #03
+Test: organizeChristmasDinner([
+        ["fruit salad", "apple", "banana", "orange"],
+        ["berry smoothie", "blueberry", "banana", "milk"],
+        ["apple pie", "apple", "sugar", "flour"]
+      ])
+
+Expected:
+[
+  [
+    "apple",
+    "apple pie",
+    "fruit salad"
+  ],
+  [
+    "banana",
+    "berry smoothie",
+    "fruit salad"
+  ]
+]
+
+Actual:
+[]
+Test #04
+Test: organizeChristmasDinner([
+    ["gingerbread", "flour", "ginger", "sugar"],
+    ["glazed ham", "ham", "honey", "sugar", "vinegar"],
+    ["roast chicken", "chicken", "rosemary", "thyme", "garlic"],
+    ["vegetable soup", "carrot", "potato", "onion", "garlic"],
+    ["fruit punch", "apple juice", "orange juice", "sugar"]
+  ])
+
+Expected:
+[
+  [
+    "garlic",
+    "roast chicken",
+    "vegetable soup"
+  ],
+  [
+    "sugar",
+    "fruit punch",
+    "gingerbread",
+    "glazed ham"
+  ]
+]
+
+Actual:
+[]
+Test #05
+Test: organizeChristmasDinner([
+    ["pumpkin pie", "pumpkin", "cinnamon", "sugar", "flour"],
+    ["mashed potatoes", "potatoes", "butter", "milk"],
+    ["cinnamon rolls", "flour", "cinnamon", "butter", "sugar"],
+    ["turkey stuffing", "bread crumbs", "celery", "onion", "butter"]
+  ])
+
+Expected:
+[
+  [
+    "butter",
+    "cinnamon rolls",
+    "mashed potatoes",
+    "turkey stuffing"
+  ],
+  [
+    "cinnamon",
+    "cinnamon rolls",
+    "pumpkin pie"
+  ],
+  [
+    "flour",
+    "cinnamon rolls",
+    "pumpkin pie"
+  ],
+  [
+    "sugar",
+    "cinnamon rolls",
+    "pumpkin pie"
+  ]
+]
+
+Actual:
+[]
+Test #06
+Test: organizeChristmasDinner([
+    ["chicken alfredo", "chicken", "pasta", "parmesan"],
+    ["parmesan chicken", "chicken", "parmesan", "bread crumbs"],
+    ["pasta salad", "pasta", "olive oil", "tomato"],
+    ["tomato soup", "tomato", "basil", "cream"]
+  ])
+
+Expected:
+[
+  [
+    "chicken",
+    "chicken alfredo",
+    "parmesan chicken"
+  ],
+  [
+    "parmesan",
+    "chicken alfredo",
+    "parmesan chicken"
+  ],
+  [
+    "pasta",
+    "chicken alfredo",
+    "pasta salad"
+  ],
+  [
+    "tomato",
+    "pasta salad",
+    "tomato soup"
+  ]
+]
+
+Actual:
+[]
+Test #07
+Test: organizeChristmasDinner([
+        ["snowflake cookies", "flour", "sugar", "vanilla"],
+        ["winter stew", "beef", "carrots", "potatoes"],
+        ["holiday punch", "cranberry juice", "orange juice", "sparkling water"],
+        ["festive salad", "lettuce", "cranberries", "walnuts"]
+      ])
+
+Expected:
+[]
+
+Actual:
+[]
+Test #08 secret
+Test #09 secret
+Test #10 secret
+Test #11 secret
+
+*/
