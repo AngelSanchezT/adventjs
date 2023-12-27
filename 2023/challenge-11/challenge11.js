c15da50984a66b95f28c4545987bb09af9450b19
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-26 22:32:45 -0500

    ✅ Solution Challenge 11 - V1

--- 2023/challenge-11/challenge11.js
+++ 2023/challenge-11/challenge11.js
@@ -1,5 +1,25 @@
 function getIndexsForPalindrome(word) {
-    return [0, 0]
+  let wordArray = word.split("");
+  let word2 = [...wordArray].reverse().join("");
+
+  if (word === word2) {
+    return [];
+  }
+
+  for (let i = 0; i < wordArray.length; i++) {
+    for (let j = i; j < wordArray.length; j++) {
+      let wordArrayTemp = [...wordArray];
+      let letterTemp = wordArrayTemp[i];
+      wordArrayTemp[i] = wordArrayTemp[j];
+      wordArrayTemp[j] = letterTemp;
+
+      if (wordArrayTemp.join("") === wordArrayTemp.reverse().join("")) {
+        return [i, j];
+      }
+    }
+  }
+
+  return null;
 }
 
 module.exports = getIndexsForPalindrome;
--- 2023/challenge-11/challenge11.test.js
+++ 2023/challenge-11/challenge11.test.js
@@ -8,64 +8,64 @@ describe('Challenge #11: 📖 The studious elves', () => {
     it("Test #02: getIndexsForPalindrome('anna') | Expected: []", () =>{
         const result = getIndexsForPalindrome('anna');
         const expected = [];
-
-        expect(result).toBe(expected);
+        console.log("TEST02: result: ", result)
+        expect(result).toEqual(expected);
     });
 
     it("Test #03: getIndexsForPalindrome('abab') | Expected: [0,1]", () =>{
         const result = getIndexsForPalindrome('abab');
         const expected = [0,1];
 
-        expect(result).toBe(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #04: getIndexsForPalindrome('abac') | Expected: null", () =>{
         const result = getIndexsForPalindrome('abac');
         const expected = null;
 
-        expect(result).toBe(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #05: getIndexsForPalindrome('aaaaaaaa') | Expected: []", () =>{
         const result = getIndexsForPalindrome('aaaaaaaa');
         const expected = [];
 
-        expect(result).toBe(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #06: getIndexsForPalindrome('aaababa') | Expected: [1,3]", () =>{
         const result = getIndexsForPalindrome('aaababa');
         const expected = [1,3];
 
-        expect(result).toBe(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #07: getIndexsForPalindrome('caababa') | Expected: null", () =>{
         const result = getIndexsForPalindrome('caababa');
         const expected = null;
 
-        expect(result).toBe(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #08: getIndexsForPalindrome('rotavator') | Expected: []", () =>{
         const result = getIndexsForPalindrome('rotavator');
         const expected = [];
 
-        expect(result).toBe(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #09: getIndexsForPalindrome('rotaratov') | Expected: [4,8]", () =>{
         const result = getIndexsForPalindrome('rotaratov');
         const expected = [4,8];
 
-        expect(result).toBe(expected);
+        expect(result).toEqual(expected);
     });
 
     it("Test #10: getIndexsForPalindrome('saippuakivikauppias') | Expected: []", () =>{
         const result = getIndexsForPalindrome('saippuakivikauppias');
         const expected = [];
 
-        expect(result).toBe(expected);
+        expect(result).toEqual(expected);
     });
 
 });
