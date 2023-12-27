f501eeb51c1598690456ad783e17963aa9e3325c
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-26 21:10:16 -0500

    Initial Challenge 25

---
+++ 2023/challenge-25/challenge25.test.js
@@ -0,0 +1,90 @@
+
+/*
+Tests failed
+Test #01
+Test: return type
+
+Expected:
+"number"
+
+Actual:
+"number"
+Test #02
+Test: travelDistance(
+`.....1....
+..S.......
+..........
+....3.....
+......2...`
+)
+
+Expected:
+12
+
+Actual:
+0
+Test #03
+Test: travelDistance(`..S.1...`)
+
+Expected:
+2
+
+Actual:
+0
+Test #04
+Test: travelDistance(
+`.....2....
+..S.......
+..........
+....1.....
+......3...`
+)
+
+Expected:
+13
+
+Actual:
+0
+Test #05
+Test: travelDistance(
+`3....1....
+..S.......
+.........2
+..........
+......4...`
+)
+
+Expected:
+31
+
+Actual:
+0
+Test #06
+Test: travelDistance(`S1`)
+
+Expected:
+1
+
+Actual:
+0
+Test #07
+Test: travelDistance(`1....S`)
+
+Expected:
+5
+
+Actual:
+0
+Test #08
+Test: travelDistance(`S12....3`)
+
+Expected:
+7
+
+Actual:
+0
+Test #09 secret
+Test #10 secret
+Test #11 secret
+Test #12 secret
+*/
