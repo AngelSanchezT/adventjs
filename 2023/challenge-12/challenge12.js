868fab86ea6dcc554013bd94d4044ce4c3c5d62d
Angel Sanchez <ajsanchez22@gmail.com>
2023-12-30 01:10:13 -0500

    ✅ Solution Challenge 12

--- 2023/challenge-12/challenge12.js
+++ 2023/challenge-12/challenge12.js
@@ -1,5 +1,31 @@
 function checkIsValidCopy(original, copy) {
-    return true
+  const CHARACTERS = "#+:. ";
+  const NO_DEGRADE = "0123456789 ";
+  const temp = original.toLowerCase();
+
+  for (let i = 0; i < original.length; i++) {
+    const isCharacterNoDegrade = NO_DEGRADE.includes(temp[i]);
+    const isCharacterNoEquals = temp[i] !== copy[i];
+    const isCharacterDegrade = CHARACTERS.includes(temp[i]);
+    const isCharacterErrorCopy =
+      CHARACTERS.indexOf(temp[i]) > CHARACTERS.indexOf(copy[i]);
+    const isOriginalEqualsCopy = original[i] === copy[i];
+    const isCopyDegrade = CHARACTERS.includes(copy[i]);
+
+    if (isCharacterNoDegrade && isCharacterNoEquals) {
+      return false;
+    }
+
+    if (isCharacterDegrade && isCharacterNoEquals && isCharacterErrorCopy) {
+      return false;
+    }
+
+    if (!(isOriginalEqualsCopy || !isCharacterNoEquals || isCopyDegrade)) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = checkIsValidCopy;
