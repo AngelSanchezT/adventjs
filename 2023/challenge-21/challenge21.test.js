9fe9b2b1f4608bfd93e1b974d250de11e637dfcf
Angel Sanchez <ajsanchez22@gmail.com>
2024-01-02 23:36:28 -0500

    Add Unit Testing Challenge 21

--- 2023/challenge-21/challenge21.test.js
+++ 2023/challenge-21/challenge21.test.js
@@ -1,103 +1,65 @@
-const findBalancedSegment = require('./challenge21');
+const findBalancedSegment = require("./challenge21");
 
 describe("Challenge #21: 🪐 Binary message", () => {
   it("Test #01: return type", () => {
-    expect(typeof findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1])).toEqual("object");
+    expect(typeof findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1])).toEqual(
+      "object"
+    );
+  });
+
+  it("Test #02: findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1])", () => {
+    const result = findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1]);
+    const expected = [2, 5];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #03: findBalancedSegment([1, 1, 0])", () => {
+    const result = findBalancedSegment([1, 1, 0]);
+    const expected = [1, 2];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #04: findBalancedSegment([1, 1, 1])", () => {
+    const result = findBalancedSegment([1, 1, 1]);
+    const expected = [];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #05: findBalancedSegment([1, 0, 1])", () => {
+    const result = findBalancedSegment([1, 0, 1]);
+    const expected = [0, 1];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #06: findBalancedSegment([1, 0, 1, 0])", () => {
+    const result = findBalancedSegment([1, 0, 1, 0]);
+    const expected = [0, 3];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #07: findBalancedSegment([1, 1, 0, 1, 0, 1])", () => {
+    const result = findBalancedSegment([1, 1, 0, 1, 0, 1]);
+    const expected = [1, 4];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #08: findBalancedSegment([1, 0, 0, 0, 1, 1, 1, 0, 0, 0])", () => {
+    const result = findBalancedSegment([1, 0, 0, 0, 1, 1, 1, 0, 0, 0]);
+    const expected = [0, 7];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #09: findBalancedSegment([0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1])", () => {
+    const result = findBalancedSegment([0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1]);
+    const expected = [5, 10];
+
+    expect(result).toEqual(expected);
   });
 });
-
-/*
-Test #01
-Test: return type
-
-Expected:
-"array"
-
-Actual:
-"array"
-Test #02
-Test: findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1])
-
-Expected:
-[
-  2,
-  5
-]
-
-Actual:
-[]
-Test #03
-Test: findBalancedSegment([1, 1, 0])
-
-Expected:
-[
-  1,
-  2
-]
-
-Actual:
-[]
-Test #04
-Test #05
-Test: findBalancedSegment([1, 0, 1])
-
-Expected:
-[
-  0,
-  1
-]
-
-Actual:
-[]
-Test #06
-Test: findBalancedSegment([1, 0, 1, 0])
-
-Expected:
-[
-  0,
-  3
-]
-
-Actual:
-[]
-Test #07
-Test: findBalancedSegment([1, 1, 0, 1, 0, 1])
-
-Expected:
-[
-  1,
-  4
-]
-
-Actual:
-[]
-Test #08
-Test: findBalancedSegment([1, 0, 0, 0, 1, 1, 1, 0, 0, 0])
-
-Expected:
-[
-  0,
-  7
-]
-
-Actual:
-[]
-Test #09
-Test: findBalancedSegment([0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1])
-
-Expected:
-[
-  5,
-  10
-]
-
-Actual:
-[]
-Test #10 secret
-Test #11 secret
-Test #12 secret
-Test #13 secret
-Test #14 secret
-Test #15 secret
-
-*/
