5c57bea0a4efdad5f071ee8908a5fb50065b4ab9
Angel Sanchez <ajsanchez22@gmail.com>
2024-01-02 23:25:35 -0500

    Add Unit Testing Challenge 20

--- 2023/challenge-20/challenge20.test.js
+++ 2023/challenge-20/challenge20.test.js
@@ -1,241 +1,100 @@
-const distributeGifts = require('./challenge20');
+const distributeGifts = require("./challenge20");
 
-describe("Challenge #20: 🏋️‍♂️ Distribute the weight",() => {
+describe("Challenge #20: 🏋️‍♂️ Distribute the weight", () => {
   it("Test #01: return type", () => {
-    expect(typeof distributeGifts([
+    expect(
+      typeof distributeGifts([
+        [4, 5, 1],
+        [6, null, 3],
+        [8, null, 4],
+      ])
+    ).toEqual("object");
+  });
+
+  it("Test #02: distributeGifts([[4, 5, 1],[6, null, 3],[8, null, 4]])", () => {
+    const result = distributeGifts([
       [4, 5, 1],
       [6, null, 3],
-      [8, null, 4]
-    ])).toEqual("object");
+      [8, null, 4],
+    ]);
+    const expected = [
+      [5, 3, 3],
+      [6, 5, 3],
+      [7, 6, 4],
+    ];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #03: distributeGifts([[2, null],[null, 3]])", () => {
+    const result = distributeGifts([
+      [2, null],
+      [null, 3],
+    ]);
+
+    const expected = [
+      [2, 3],
+      [3, 3],
+    ];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #04: distributeGifts([[2, 1, 1],[3, 4, null]])", () => {
+    const result = distributeGifts([
+      [2, 1, 1],
+      [3, 4, null],
+    ]);
+
+    const expected = [
+      [2, 2, 1],
+      [3, 3, 3],
+    ];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #05: distributeGifts([[null, 5],[3, null]])", () => {
+    const result = distributeGifts([
+      [null, 5],
+      [3, null],
+    ]);
+
+    const expected = [
+      [4, 5],
+      [3, 4],
+    ];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #06: distributeGifts([[1, 2, 3],[4, 5, 6],[7, 8, 9]])", () => {
+    const result = distributeGifts([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ]);
+
+    const expected = [
+      [2, 3, 4],
+      [4, 5, 6],
+      [6, 7, 8],
+    ];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Test #07: distributeGifts([[null, 1, null, 1, null],[1, null, 1, null, 1],])", () => {
+    const result = distributeGifts([
+      [null, 1, null, 1, null],
+      [1, null, 1, null, 1],
+    ]);
+
+    const expected = [
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+    ];
+
+    expect(result).toEqual(expected);
   });
 });
-
-/*
-
-Test #02
-Test: distributeGifts([
-  [4, 5, 1],
-  [6, null, 3],
-  [8, null, 4]
-])
-
-Expected:
-[
-  [
-    5,
-    3,
-    3
-  ],
-  [
-    6,
-    5,
-    3
-  ],
-  [
-    7,
-    6,
-    4
-  ]
-]
-
-Actual:
-[
-  [
-    4,
-    5,
-    1
-  ],
-  [
-    6,
-    null,
-    3
-  ],
-  [
-    8,
-    null,
-    4
-  ]
-]
-Test #03
-Test: distributeGifts([
-  [2, null],
-  [null, 3]
-])
-
-Expected:
-[
-  [
-    2,
-    3
-  ],
-  [
-    3,
-    3
-  ]
-]
-
-Actual:
-[
-  [
-    2,
-    null
-  ],
-  [
-    null,
-    3
-  ]
-]
-Test #04
-Test: distributeGifts([
-  [2, 1, 1],
-  [3, 4, null]
-])
-
-Expected:
-[
-  [
-    2,
-    2,
-    1
-  ],
-  [
-    3,
-    3,
-    3
-  ]
-]
-
-Actual:
-[
-  [
-    2,
-    1,
-    1
-  ],
-  [
-    3,
-    4,
-    null
-  ]
-]
-Test #05
-Test: distributeGifts([
-  [null, 5],
-  [3, null]
-])
-
-Expected:
-[
-  [
-    4,
-    5
-  ],
-  [
-    3,
-    4
-  ]
-]
-
-Actual:
-[
-  [
-    null,
-    5
-  ],
-  [
-    3,
-    null
-  ]
-]
-Test #06
-Test: distributeGifts([
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-])
-
-Expected:
-[
-  [
-    2,
-    3,
-    4
-  ],
-  [
-    4,
-    5,
-    6
-  ],
-  [
-    6,
-    7,
-    8
-  ]
-]
-
-Actual:
-[
-  [
-    1,
-    2,
-    3
-  ],
-  [
-    4,
-    5,
-    6
-  ],
-  [
-    7,
-    8,
-    9
-  ]
-]
-Test #07
-Test: distributeGifts([
-  [null, 1, null, 1, null],
-  [1, null, 1, null, 1],
-])
-
-Expected:
-[
-  [
-    1,
-    1,
-    1,
-    1,
-    1
-  ],
-  [
-    1,
-    1,
-    1,
-    1,
-    1
-  ]
-]
-
-Actual:
-[
-  [
-    null,
-    1,
-    null,
-    1,
-    null
-  ],
-  [
-    1,
-    null,
-    1,
-    null,
-    1
-  ]
-]
-Test #08 secret
-Test #09 secret
-Test #10 secret
-Test #11 secret
-
-
-*/
