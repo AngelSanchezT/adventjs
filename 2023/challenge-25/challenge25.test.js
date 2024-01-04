8069b231831743b5c18bc3a52ff22151d700106d
Angel Sanchez <ajsanchez22@gmail.com>
2024-01-03 23:14:59 -0500

    Add Unit Test for Challenge 25

--- 2023/challenge-25/challenge25.test.js
+++ 2023/challenge-25/challenge25.test.js
@@ -1,106 +1,75 @@
-const travelDistance = require('./challenge25');
+const travelDistance = require("./challenge25");
 
 describe("Challenge #25: 🗺️ Calculating distances", () => {
-    it("Test #01: return type", () => {
-        expect(typeof travelDistance(
-            `.....1....
+  it("Test #01: return type", () => {
+    expect(
+      typeof travelDistance(
+        `.....1....
             ..S.......
             ..........
             ....3.....
             ......2...`
-            )).toEqual("number");
-    });
+      )
+    ).toEqual("number");
+  });
 
-    
+  it("Test #02: Expected: 12", () => {
+    const result = travelDistance(
+      `.....1....
+            ..S.......
+            ..........
+            ....3.....
+            ......2...`
+    );
+    const expect = 12;
+    expect(result).toEqual(expect);
+  });
 
+  it("Test #03: travelDistance(`..S.1...`) | Expected: 2", () => {
+    const result = travelDistance(`..S.1...`);
+    const expect = 2;
+    expect(result).toEqual(expect);
+  });
 
+  it("Test #04: Expected: 13", () => {
+    const result = travelDistance(
+      `.....2....
+        ..S.......
+        ..........
+        ....1.....
+        ......3...`
+    );
+    const expect = 13;
+    expect(result).toEqual(expect);
+  });
+
+  it("Test #05: Expected: 31", () => {
+    const result = travelDistance(
+      `3....1....
+        ..S.......
+        .........2
+        ..........
+        ......4...`
+    );
+    const expect = 31;
+    expect(result).toEqual(expect);
+  });
+
+  it("Test #06: travelDistance(`S1`) | Expected: 1", () => {
+    const result = travelDistance(`S1`);
+    const expect = 1;
+    expect(result).toEqual(expect);
+  });
+
+  it("Test #07: travelDistance(`1....S`) | Expected: 5", () => {
+    const result = travelDistance(`1....S`);
+    const expect = 5;
+    expect(result).toEqual(expect);
+  });
+
+  it("Test #08: travelDistance(`S12....3`) | Expected: 7", () => {
+    const result = travelDistance(`S12....3`);
+    const expect = 7;
+    expect(result).toEqual(expect);
+  });
 });
-/*
-Tests failed
-Test #01
-Test: return type
-
-Expected:
-"number"
-
-Actual:
-"number"
-Test #02
-Test: travelDistance(
-`.....1....
-..S.......
-..........
-....3.....
-......2...`
-)
-
-Expected:
-12
-
-Actual:
-0
-Test #03
-Test: travelDistance(`..S.1...`)
-
-Expected:
-2
-
-Actual:
-0
-Test #04
-Test: travelDistance(
-`.....2....
-..S.......
-..........
-....1.....
-......3...`
-)
-
-Expected:
-13
-
-Actual:
-0
-Test #05
-Test: travelDistance(
-`3....1....
-..S.......
-.........2
-..........
-......4...`
-)
-
-Expected:
-31
-
-Actual:
-0
-Test #06
-Test: travelDistance(`S1`)
-
-Expected:
-1
-
-Actual:
-0
-Test #07
-Test: travelDistance(`1....S`)
-
-Expected:
-5
-
-Actual:
-0
-Test #08
-Test: travelDistance(`S12....3`)
-
-Expected:
-7
-
-Actual:
-0
-Test #09 secret
-Test #10 secret
-Test #11 secret
-Test #12 secret
-*/
