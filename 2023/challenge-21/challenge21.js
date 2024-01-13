e39fd907319af299973101f974997192768c7f6b
Angel Sanchez <ajsanchez22@gmail.com>
2024-01-13 16:29:03 -0500

    Initial Challenge: ❌ 4 failed, ✅5 passed, 9 total

--- 2023/challenge-21/challenge21.js
+++ 2023/challenge-21/challenge21.js
@@ -1,5 +1,29 @@
 function findBalancedSegment(message) {
-  return [];
+  let maxLen = 0;
+  let startIndex = -1;
+  let currentIndex = -1;
+  let countZeros = 0;
+  let countOnes = 0;
+
+  for (let i = 0; i < message.length; i++) {
+    if (message[i] === 0) {
+      countZeros++;
+    } else {
+      countOnes++;
+    }
+
+    if (countZeros === countOnes && countZeros + countOnes > maxLen) {
+      maxLen = countZeros + countOnes;
+      startIndex = currentIndex + 1;
+    } else if (countZeros === countOnes) {
+      currentIndex = i;
+      countZeros = 0;
+      countOnes = 0;
+    }
+  }
+
+  return maxLen > 0 ? [startIndex, startIndex + maxLen - 1] : [];
 }
+
 
 module.exports = findBalancedSegment;
