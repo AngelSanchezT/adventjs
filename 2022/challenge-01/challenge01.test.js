007f5a6a54b56ba0f899682f35fa8a9c8d131d4f
AngelSanchezT <ajsanchez22@gmail.com>
2024-01-15 23:44:47 -0500

    Add Unit Testing Challenge 01 - 2022

---
+++ 2022/challenge-01/challenge01.test.js
@@ -0,0 +1,57 @@
+
+
+
+/* 
+
+Test #01
+Test: return type
+
+Expected:
+"array"
+
+Actual:
+"array"
+Test #02
+Test: wrapping(gifts)
+
+Expected:
+[
+  "*****\n*cat*\n*****",
+  "******\n*game*\n******",
+  "*******\n*socks*\n*******"
+]
+
+Actual:
+[]
+Test #03
+Test: wrapping(["midu"])
+
+Expected:
+[
+  "******\n*midu*\n******"
+]
+
+Actual:
+[]
+Test #04
+Test: wrapping(["a"])
+
+Expected:
+[
+  "***\n*a*\n***"
+]
+
+Actual:
+[]
+Test #05
+Test: wrapping an empty array should return an empty array
+
+Expected:
+[]
+
+Actual:
+[]
+Test #06 (secret 🔒)
+Test #07 (secret 🔒)
+
+*/
