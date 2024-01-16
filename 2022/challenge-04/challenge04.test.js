ad33022673e2136d773fbc0da82014f137f87254
AngelSanchezT <ajsanchez22@gmail.com>
2024-01-15 23:57:04 -0500

    Initial Challenge 04 - 2022

---
+++ 2022/challenge-04/challenge04.test.js
@@ -0,0 +1,77 @@
+/*
+Han fallado los tests
+Test #01
+Test: return type
+
+Expected:
+"boolean"
+
+Actual:
+"boolean"
+Test #02
+Test: fitsInOneBox([
+  { l: 1, w: 1, h: 10 },
+  { l: 3, w: 3, h: 12 },
+  { l: 2, w: 2, h: 1 },
+])
+
+Expected:
+false
+
+Actual:
+false
+Test #03
+Test: fitsInOneBox([
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 }
+])
+
+Expected:
+true
+
+Actual:
+false
+Test #04
+Test: fitsInOneBox([
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 }
+])
+
+Expected:
+false
+
+Actual:
+false
+Test #05
+Test: fitsInOneBox([
+        { l: 1, w: 1, h: 1 },
+        { l: 2, w: 2, h: 2 },
+        { l: 2, w: 10, h: 2}
+      ])
+
+Expected:
+false
+
+Actual:
+false
+Test #06
+Test: fitsInOneBox([
+        { l: 1, w: 1, h: 1 },
+        { l: 3, w: 3, h: 3 },
+        { l: 2, w: 2, h: 2 }
+      ])
+
+Expected:
+true
+
+Actual:
+false
+Test #07 (secret 🔒)
+Test #08 (secret 🔒)
+Test #09 (secret 🔒)
+Test #10 (secret 🔒)
+
+
+
+*/
