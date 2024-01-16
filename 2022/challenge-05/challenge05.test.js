68594ecdc9e18c186a16b95cfc987dd77c1867ec
AngelSanchezT <ajsanchez22@gmail.com>
2024-01-16 00:00:05 -0500

    Add unit tsest challentege 05 -2022

---
+++ 2022/challenge-05/challenge05.test.js
@@ -0,0 +1,79 @@
+/*
+Han fallado los tests
+Test #01
+Test: return type
+
+Expected:
+"number"
+
+Actual:
+"number"
+Test #02
+Test: getMaxGifts([12, 3, 11, 5, 7], 20, 3)
+
+Expected:
+20
+
+Actual:
+0
+Test #03
+Test: getMaxGifts([50], 15, 1)
+
+Expected:
+0
+
+Actual:
+0
+Test #04
+Test: getMaxGifts([50], 100, 1)
+
+Expected:
+50
+
+Actual:
+0
+Test #05
+Test: getMaxGifts([50, 70], 100, 1)
+
+Expected:
+70
+
+Actual:
+0
+Test #06
+Test: getMaxGifts([50, 70, 30], 100, 2)
+
+Expected:
+100
+
+Actual:
+0
+Test #07
+Test: getMaxGifts([50, 70, 30], 100, 3)
+
+Expected:
+100
+
+Actual:
+0
+Test #08
+Test: getMaxGifts([50, 10, 40, 1000, 500, 200], 199, 4)
+
+Expected:
+100
+
+Actual:
+0
+Test #09
+Test: getMaxGifts([50, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 1000, 1000)
+
+Expected:
+115
+
+Actual:
+0
+Test #10 (secret 🔒)
+Test #11 (secret 🔒)
+Test #12 (secret 🔒)
+Test #13 (secret 🔒)
+*/
