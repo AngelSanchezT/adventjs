968ae140b6c4c791b2e7a6aa27f6fc73920da050
AngelSanchezT <ajsanchez22@gmail.com>
2024-02-03 00:40:58 -0500

    ✅ Solution Challenge 07 - 2022

--- 2022/challenge-07/challenge07.js
+++ 2022/challenge-07/challenge07.js
@@ -1,5 +1,16 @@
 function getGiftsToRefill(a1, a2, a3) {
-    return []
+    let inventory = new Set([a1, a2, a3].flat());
+    let refill = [];
+
+    inventory.forEach((gift) => {
+        let sum = a1.includes(gift) + a2.includes(gift) + a3.includes(gift);
+        console.log(sum);
+        if (sum === 1) {
+            refill.push(gift);
+        }
+    });
+
+    return refill;
 }
 
 module.exports = getGiftsToRefill;
